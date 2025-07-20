7b56fe83a59261e06e10cab8589219be34034f04
RubaaAlghamdi <ralghamdi1255@stu.kau.edu.sa>
2025-07-20 22:56:07 +0300

    Ruba; i've done scheduler class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
